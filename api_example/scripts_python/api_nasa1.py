7c00fdd1aa86d457de2adc9e01cdd0ae2fce9a50
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-09-02 21:56:22 -0500

    Create api_nasa1.py

---
+++ api_example/scripts_python/api_nasa1.py
@@ -0,0 +1,6 @@
+'''
+API: Aplication Programming Interface
+NASA API: https://api.nasa.gov/
+Developer: David Felipe Gustin Rivas
+API URL: https://api.nasa.gov/neo/rest/v1/neo/3726712?api_key={X0Z2z05tz7an956stiO1IUefIHpxeazEVUjmiAgQ}
+'''
