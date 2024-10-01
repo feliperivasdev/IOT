f5802d7e5731867b6e8d9cef8c9b611063140ff7
Felipe Rivas <dfgustin.4560@unicesmag.edu.co>
2024-09-30 21:44:37 -0500

    Update orion.py

--- api_example/scripts_python/orion.py
+++ api_example/scripts_python/orion.py
@@ -5,8 +5,29 @@ import os
 import requests
 
 #comands for cleaning console output
-os.system('clear')
+#os.system('clear')
 
 #Define the function
 def getData():
-    print
+    print('::: SOLAR SYSTEM INFO :::')
+    url = 'https://api.le-systeme-solaire.net/rest/bodies/?filter%5B%5D=isComet'
+
+    try:
+        #request body to API server
+        response = requests.get(url)
+        response.raise_for_status()
+
+        #Convert the response to JSON   
+        data = response.json()
+        
+        #print the data
+        print(data)
+
+        #check if the request was successful
+        print('Request successful!')
+
+    except requests.exceptions.RequestException as e:
+        print(f'Error: {e}')
+        return
+    
+    getData()
